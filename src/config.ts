9bfdbd23f18048f0c043eb60e55564716b43a1c9
スタートdev <progress2012start@gmail.com>
2026-07-13 16:29:38 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const APP_VERSION = '0.1.0';
 
 // GAS Web App URLを設定してください。例: https://script.google.com/macros/s/.../exec
-export const GAS_WEB_APP_URL = '';
+export const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzcDoEuB2mRDJ_rqk2fwAxE5R6Xvyv1DiuH3s4LhONJ2O4XEFuKorrn9fXUxgh1Hp_b9w/exec';
